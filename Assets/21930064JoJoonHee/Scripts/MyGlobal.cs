11fcec7536fda104be38fa10e5640e63bf6c4185
jjh <jhj9490@gmail.com>
2020-04-24 16:38:38 +0900

    CHECK BRANCH

---
+++ Assets/21930064JoJoonHee/Scripts/MyGlobal.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MYGLOBAL
+{
+    public class MyGlobal : MonoBehaviour
+    {
+        public class Input
+        {
+            // 인풋매니져에 지정해둔 이름 지정
+            public const string BUTTON_HORIZONTAL_MOVE = "Horizontal";
+            public const string BUTTON_VERTICAL_MOVE = "Vertical";
+            public const string BUTTON_JUMP = "Jump";
+            public const string BUTTON_DASH = "Fire1";
+        }
+    }
+
+    public class MyGlobals
+    {
+        public const string PACKAGE_NAME = "MYGLOBAL";
+
+        public static int GetFrameCount(float time)
+        {
+            float frames = time / Time.fixedDeltaTime;
+            int roundedFrames = Mathf.RoundToInt(frames);
+
+            if (Mathf.Approximately(frames, roundedFrames))
+            {
+                return roundedFrames;
+            }
+
+            return Mathf.CeilToInt(frames);
+        }
+    }
+}
+
+
+
