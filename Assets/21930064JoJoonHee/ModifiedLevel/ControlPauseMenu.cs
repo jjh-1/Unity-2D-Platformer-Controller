d0d100f3ee9a59c8e29b432fd1cd7ee47dfc1976
jjh <jhj9490@gmail.com>
2020-04-27 21:25:03 +0900

    게임 일시정지, 재개 구현

---
+++ Assets/21930064JoJoonHee/ModifiedLevel/ControlPauseMenu.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlPauseMenu : MonoBehaviour
+{
+    // 액티브 <-> 디액티브 할것
+    public GameObject pauseMenu;
+
+    // 복) 스태틱 붙인 변수는 인스턴스마다 따로 있는게 아니라 단 하나
+    public static bool isGamePaused = false;
+
+    // 일시정지 메소드
+    private void PauseGame()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0; // 0 이면 시간 멈춤
+        isGamePaused = true;
+    }
+
+    // 게임 재개 메소드
+    // 리슘 버튼에 이벤트 연결 해줘야하니 퍼블릭
+    public void ResumeGame()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1; // 1이면 원래 시간 스케일
+        isGamePaused = false;
+    }
+
+    // 모노비헤비어 메소드
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isGamePaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+}
