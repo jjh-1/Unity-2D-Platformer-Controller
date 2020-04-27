421c4ecf8d03244132c3acae5516cc99ff12430a
jjh <jhj9490@gmail.com>
2020-04-27 19:43:42 +0900

    메인메뉴 <-> 옵션메뉴 이동, 종료 버튼 구현

--- Assets/21930064JoJoonHee/MainMenu/MainMenu.cs
+++ Assets/21930064JoJoonHee/MainMenu/MainMenu.cs
@@ -10,4 +10,11 @@ public class MainMenu : MonoBehaviour
         // 일단 빌드세팅에 넣어둔 씬들 인덱스로 씬전환
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
+
+    public void QuitGame()
+    { 
+        Debug.Log("Quit Button Pressed");
+        // 유니티 에디터에선 작동 안하고 빌드해서 독립 프로그램 됬을때 작동
+        Application.Quit();
+    }
 }
