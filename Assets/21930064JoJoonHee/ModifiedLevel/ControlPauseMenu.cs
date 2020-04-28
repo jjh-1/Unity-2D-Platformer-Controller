eac8b906afb252e0717b8f6f8f1bd733eef32956
jjh <jhj9490@gmail.com>
2020-04-28 17:20:22 +0900

    타일셋 추가

--- Assets/21930064JoJoonHee/ModifiedLevel/ControlPauseMenu.cs
+++ Assets/21930064JoJoonHee/ModifiedLevel/ControlPauseMenu.cs
@@ -7,16 +7,16 @@ using UnityEngine.SceneManagement; // 씬 관리 할려면 필요한 유니티 �
 // !!!! 게임 시작시 비활성화된 오브젝트에 지정된 스크립트는 실행안되니 정지메뉴 오브젝트가 아니라 캔버스에 스크립트 놔야함
 public class ControlPauseMenu : MonoBehaviour
 {
-    // 액티브 <-> 디액티브 할것
+    // 액티브 <-> 디액티브 할 오브젝트
     public GameObject pauseMenu;
 
     // 복) 스태틱 붙인 변수는 인스턴스마다 따로 있는게 아니라 단 하나
     public static bool isGamePaused = false;
 
-    // 메인메뉴로 돌아가는 메소드
+    // 메인메뉴 씬으로 가는 메소드
     public void GotoMainMenuScene()
     {
-        Time.timeScale = 1; // ! 일시정지 메소드에서 0으로 만들었으니 다시 정상으로
+        Time.timeScale = 1; // ! 일시정지 메소드에서 0으로 만들었으니 다시 정상으로 해줘야함
         SceneManager.LoadScene("Test Main Menu"); // 하드코딩
     }
 
@@ -27,6 +27,7 @@ public class ControlPauseMenu : MonoBehaviour
         // 유니티 에디터에선 작동 안하고 빌드해서 독립 프로그램 됬을때 작동
         Application.Quit();
     }
+    // TODO : 글로벌 메소드로
 
     // 게임 재개 메소드
     // 리슘 버튼에 이벤트 연결 해줘야하니 퍼블릭
