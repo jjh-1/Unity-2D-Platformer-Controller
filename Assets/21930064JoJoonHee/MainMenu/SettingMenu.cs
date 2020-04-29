79848a52d57a339678bae37537229977c5b44c78
jjh <jhj9490@gmail.com>
2020-04-29 22:42:26 +0900

    소리설정 글로벌 저장 구현

--- Assets/21930064JoJoonHee/MainMenu/SettingMenu.cs
+++ Assets/21930064JoJoonHee/MainMenu/SettingMenu.cs
@@ -4,19 +4,31 @@ using UnityEngine;
 using UnityEngine.Audio; // @ 오디오관련
 using UnityEngine.UI;
 using TMPro;
+using TRANSGLOBAL; // 셋팅관련 변수 글로벌로 관리
 
 public class SettingMenu : MonoBehaviour
 {
+    // 셋팅 글로벌로 저장되게함
+    public Slider volumeSlider;
+    private void Awake()
+    {
+        volumeSlider.value = TransGlobal.volume;
+        SetVolume(TRANSGLOBAL.TransGlobal.volume);
+    }
+    
     // ! 이벤트에 연결해줄거니 메소드들 퍼블릭이어야함 !
 
     #region 오디오 믹서 컨트롤 관련 변수, 메소드
-    // ? 인스펙터에서 레퍼런스 지정 ?
+    // 인스펙터에서 레퍼런스 지정
     public AudioMixer audioMixer;
     // @!!!! 메소드의 파라미터가 float 이면 슬라이더의 유니티 이벤트 핸들러에서 다이나믹 플롯 옵션으로 이 메소드에 값주며 지정가능
-    public void SetVolume(float volume)
+    public void SetVolume(float newVolume)
     {
+        // 볼륨셋팅 글로벌 저장
+        TRANSGLOBAL.TransGlobal.volume = newVolume;
+
         // @ 첫번째 아규먼트는 유니티 믹서에서 옵션으로 노출시킨 볼륨 변수 커스텀 이름
-        audioMixer.SetFloat("ExposedVolume", volume);
+        audioMixer.SetFloat("ExposedVolume", newVolume);
     }
     #endregion
 
@@ -85,4 +97,6 @@ public class SettingMenu : MonoBehaviour
         Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);       
     }
     #endregion
+
+
 }
--- Assets/21930064JoJoonHee/MyScripts/TransGlobal.cs
+++ Assets/21930064JoJoonHee/MyScripts/TransGlobal.cs
@@ -2,23 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TransGlobal : MonoBehaviour
+namespace TRANSGLOBAL // 셋팅관련 변수 글로벌로 관리
 {
-    private static TransGlobal audioManager;
-
-    private void Awake()
+    public class TransGlobal : MonoBehaviour
     {
-        // 씬 전환시 해당 오브젝트는 그대로 남음
-        DontDestroyOnLoad(this);
+        // 셋팅관련 변수 글로벌로 관리
+        public static float volume = 0;
 
-        // 다시 메인화면씬 로드시 오디오메니저 다시 생기는거 방지
-        if(audioManager == null)
+
+        // 오디오 관련
+        private static TransGlobal audioManager;
+        private void Awake()
         {
-            audioManager = this;
-        }
-        else
-        {
-            DestroyObject(gameObject);
+            // 씬 전환시 해당 오브젝트는 그대로 남음
+            DontDestroyOnLoad(this);
+
+            // 다시 메인화면씬 로드시 오디오메니저 다시 생기는거 방지
+            if (audioManager == null)
+            {
+                audioManager = this;
+            }
+            else
+            {
+                DestroyObject(gameObject);
+            }
         }
     }
 }
+
