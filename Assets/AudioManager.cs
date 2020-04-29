d147ef1ade0a593b02391caf84e6a4abcfaa325f
jjh <jhj9490@gmail.com>
2020-04-29 16:21:05 +0900

    씬 넘어가도 배경음 그대로 나오도록 오디오매니져 스크립트 수정

--- Assets/AudioManager.cs
+++ Assets/AudioManager.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private static AudioManager audioManager;
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        // 씬 전환시 해당 오브젝트는 그대로 남음
+        DontDestroyOnLoad(this);
+
+        // 다시 메인화면씬 로드시 오디오메니저 다시 생기는거 방지
+        if(audioManager == null)
+        {
+            audioManager = this;
+        }
+        else
+        {
+            DestroyObject(gameObject);
+        }
     }
 }
