8b87293293d3d80a45bd5add8d9bfd87ada3b9a7
jjh <jhj9490@gmail.com>
2020-06-03 20:09:49 +0900

    페이드인/아웃 구현

---
+++ Assets/21930064JoJoonHee/MainMenu/SceneLoader.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public Animator fadeAnimator; // 인스펙터에서 지정
+
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            LoadNextLevel();
+        }
+    }
+
+    // 게임시작 버튼이나 특정 트리거 발동시 다음 씬 불러오게할 메소드
+    public void LoadNextLevel()
+    {
+        StartCoroutine(ScreenFade(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
+    IEnumerator ScreenFade(int sceneBuildIndex)
+    {
+        // 페이드 애니메이션 시작
+        fadeAnimator.SetTrigger("StartFade");
+
+        // 해당 시간동안 대기
+        // 고칠것 : 리터럴 대신 애니메이션에서 시간 가져오기
+        yield return new WaitForSeconds(1f);
+
+        // 다음 씬 불러옴
+        SceneManager.LoadScene(sceneBuildIndex);
+    }
+}
