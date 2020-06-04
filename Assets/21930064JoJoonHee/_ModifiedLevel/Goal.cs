27d3b6e0fb86077cfbc285b4bee72aa03d19b416
jjh <jhj9490@gmail.com>
2020-06-04 12:18:40 +0900

    골에 도착하면 다음 씬으로 넘어가는거 구현

---
+++ Assets/21930064JoJoonHee/_ModifiedLevel/Goal.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goal : MonoBehaviour
+{
+    public SceneLoader sceneLoader;
+
+    // 리지드바디 컴포넌트 있어야 작동함
+    // 골의 콜라이더에 플레이어가 들어가면 다음 씬 불러옴
+    void OnTriggerEnter2D(Collider2D o)
+    {
+        if(o.name == "TestPlayer")
+        {
+            sceneLoader.LoadNextLevel();
+        }
+        
+    }
+
+}
