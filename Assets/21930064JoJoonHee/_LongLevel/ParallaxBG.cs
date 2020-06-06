0ac5d06b2dcec57accb9803d5fa82588e20ad013
jjh <jhj9490@gmail.com>
2020-06-06 15:58:36 +0900

    패럴랙스 스크롤링 기본구현 완료

--- Assets/21930064JoJoonHee/_LongLevel/ParallaxBG.cs
+++ Assets/21930064JoJoonHee/_LongLevel/ParallaxBG.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class ParallaxBG : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Transform camCurrTransform; // 카메라 현위치 저장
+
+    private Vector3 camLastPosition; // 이전프레임의 카메라 위치
+
+    private void Start()
     {
-        
+        camCurrTransform = Camera.main.transform;
+        camLastPosition = camCurrTransform.position;
     }
 
-    // Update is called once per frame
-    void Update()
+    // !!!! 카메라가 움직인후 수행해야하니 레이트 업데이트 (아니면 버벅거림)
+    private void LateUpdate()
     {
-        
+        Vector3 deltaMove = camCurrTransform.position - camLastPosition;
+        transform.position += deltaMove*0.5f; // 카메라 속도보다 0.5배 느리게 배경 움직임
+        camLastPosition = camCurrTransform.position;
     }
 }
