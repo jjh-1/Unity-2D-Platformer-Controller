99c02f5dcbfc313715c71f08fa0ca0b0f3f75daf
jjh <jhj9490@gmail.com>
2020-06-07 23:37:33 +0900

    적 밟으면 반동으로 튀어올라가는 피드백 추가

--- Assets/21930064JoJoonHee/_LongLevel/KillEnemy.cs
+++ Assets/21930064JoJoonHee/_LongLevel/KillEnemy.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 
 public class KillEnemy : MonoBehaviour
 {
+    public PlatformerMotor2D motor; //인스펙터로 플레이어꺼만 지정
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Enemy")
         {
             collision.GetComponent<RatController>().killMe();
+
+            //적밟으면 반동으로 튀어오르는 피드백
+            motor.ForceJump();
         }
     }
 }
