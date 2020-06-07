0b1c998e97810052a3ba1352a0a4424e94d93b23
jjh <jhj9490@gmail.com>
2020-06-07 21:07:51 +0900

    죽을 상황일시 페이드아웃 하고 체크포인트로 이동후 페이드인 하도록함

--- Assets/21930064JoJoonHee/_LongLevel/LevelManager.cs
+++ Assets/21930064JoJoonHee/_LongLevel/LevelManager.cs
@@ -6,6 +6,8 @@ public class LevelManager : MonoBehaviour
 {
     public GameObject currCheckPoint;
 
+    public Animator fadeAnimator;
+
     private PlayerController2D player;
 
     private void Start()
@@ -15,6 +17,26 @@ public class LevelManager : MonoBehaviour
 
     public void RespawnPlayer()
     {
+        StartCoroutine(ScreenFadee());
+    }
+
+    IEnumerator ScreenFadee()
+    {
+        FindObjectOfType<DialogueManager>().isShowingDialogue = true;
+
+        // 페이드 아웃 애니메이션 시작
+        fadeAnimator.SetTrigger("StartFade");
+
+        // 해당 시간동안 대기
+        // 고칠것 : 리터럴 대신 애니메이션에서 시간 가져오기
+        yield return new WaitForSeconds(1f); // 1초 대기
+
+        // 페이드아웃 끝나면 체크포인트로 이동
         player.transform.position = currCheckPoint.transform.position;
+        FindObjectOfType<DialogueManager>().isShowingDialogue = false;
+
+        //페이드인
+        fadeAnimator.SetTrigger("EndFade");
+
     }
 }
